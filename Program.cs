de96309e594e206f3ac511b19575df416870f1af
EomTaeWook <trim0917@gmail.com>
2025-03-27 19:03:57 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using Certes;
+using CertOps.Handler;
+using CertOps.Model;
+using Dignus.DependencyInjection;
+using Dignus.DependencyInjection.Extensions;
+using Dignus.Extensions.Log;
+using Dignus.Log;
+using Dignus.Utils.Extensions;
+using System.Reflection;
+using System.Text.Json;
+
+namespace CertOps
+{
+    internal class Program
+    {
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            LogHelper.Fatal(e.ExceptionObject as Exception);
+        }
+
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            LogBuilder.Configuration(LogConfigXmlReader.Load("DignusLog.config")).Build();
+
+            using var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                cts.Cancel();
+            };
+
+            var serviceProvider = InitDependencies();
+            var handler = serviceProvider.GetService<ScheduleHandler>();
+            handler.Start(cts.Token);
+        }
+        private static IServiceProvider InitDependencies()
+        {
+            var serviceContainer = new ServiceContainer();
+            serviceContainer.RegisterDependencies(Assembly.GetExecutingAssembly());
+
+            var configFileName = "config.json";
+
+            var stage = Environment.GetEnvironmentVariable("Stage");
+
+            if(string.IsNullOrEmpty(stage) == false)
+            {
+                configFileName = $"config.{stage}.json";
+            }
+
+            var configJson = File.ReadAllText(configFileName);
+
+            var config = JsonSerializer.Deserialize<Config>(configJson);
+            serviceContainer.RegisterType(config);
+            serviceContainer.RegisterType(config.AcmeConfig);
+            serviceContainer.RegisterType(config.AzureConfig);
+
+            InitAcmeConfig(serviceContainer, config.AcmeConfig);
+
+            return serviceContainer.Build();
+        }
+        private static void InitAcmeConfig(ServiceContainer serviceContainer, AcmeConfig acmeConfig)
+        {
+            var keyPem = File.ReadAllTextAsync(acmeConfig.AccountKeyPath).GetResult();
+            var accountKey = KeyFactory.FromPem(keyPem);
+            serviceContainer.RegisterType(accountKey);
+        }
+    }
+}
---
+++ Services/AcmeService.cs
@@ -0,0 +1,62 @@
+﻿using Certes;
+using Certes.Acme;
+using Certes.Acme.Resource;
+using CertOps.Model;
+using Dignus.Collections;
+using Dignus.DependencyInjection.Attributes;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+namespace CertOps.Services
+{
+    [Injectable(Dignus.DependencyInjection.LifeScope.Transient)]
+    class AcmeService
+    {
+        private readonly AcmeContext _acmeContext;
+        private readonly ArrayQueue<string> _domains = [];
+        public AcmeService(AcmeConfig config, IKey key)
+        {
+            _acmeContext = new AcmeContext(WellKnownServers.LetsEncryptV2, key);
+            _domains.AddRange(config.Domains);
+        }
+
+        public async Task<ArrayQueue<string>> GetExpiringDomainsAsync()
+        {
+            var expiringDomains = new ArrayQueue<string>();
+
+            foreach (var domain in _domains)
+            {
+                var orderContext = _acmeContext.Order(new Uri(domain));
+                var order = await orderContext.Resource();
+
+                if (order.Status != OrderStatus.Valid)
+                {
+                    continue;
+                }
+
+                var expires = order.Expires;
+                if (expires.HasValue && expires.Value - DateTime.UtcNow < TimeSpan.FromDays(30))
+                {
+                    expiringDomains.Add(domain);
+                }
+            }
+            return expiringDomains;
+        }
+
+        public async Task<Dictionary<string, string>> GetDns01ChallengesAsync(IList<string> domains)
+        {
+            var order = await _acmeContext.NewOrder(domains);
+            var authorizations = await order.Authorizations();
+
+            var dnsChallengeRecords = new Dictionary<string, string>();
+
+            foreach (var authorization in authorizations)
+            {
+                var dnsChallenge = await authorization.Dns();
+                var dnsTxtRecordName = $"_acme-challenge.{authorization.Location.Host}";
+                var keyAuth = _acmeContext.AccountKey.DnsTxt(dnsChallenge.Token);
+                dnsChallengeRecords[dnsTxtRecordName] = keyAuth;
+            }
+            return dnsChallengeRecords;
+        }
+    }
+}
